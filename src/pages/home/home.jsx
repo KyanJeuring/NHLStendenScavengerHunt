19a8408f699d605b8069fa425538eed6ab2140ba
FlaviusPetrasciuc <flavius.petrasciuc@student.nhlstenden.com>
2025-11-13 14:55:33 +0100

    Fixed coding convention in Page1.jsx and fixed Home.jsx duplicate code

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -13,18 +13,3 @@ export default function Home() {
         </section>
     )
 }
-import React from 'react'
-import './home.css'
-
-export default function Home() {
-    return (
-        <section className="home">
-            <h1>Home</h1>
-            <p>Welcome</p>
-            <div className="logos">
-                <img src="/logos/NHLStendenENLogo.svg" alt="NHL Stenden EN Logo" className="logo"/>
-                <img src="/logos/NHLStendenNLLogo.svg" alt="NHL Stenden NL Logo" className="logo"/>
-            </div>
-        </section>
-    )
-}
--- src/pages/page1/Page1.jsx
+++ src/pages/page1/Page1.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './page1.css'
 
-export default function Page1(){
+export default function Page1() {
     return (
         <section className="page1">
             <h1>Page 1</h1>
