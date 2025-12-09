5c14f0f2d7acc8a3f0f60e58bdc8d585ba3ee386
Victoria Iascevschi <victoria.iascevschi@student.nhlstenden.com>
2025-12-09 20:10:43 +0100

    Fixed App.jsx
    Modified it so that now all pages has the same navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,19 @@
 import React from 'react'
-import { Routes, Route , useLocation} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
-import Faq from './pages/faq/Faq'
 import Navbar from './components/navbar/Navbar'
 import LearnMore from './pages/learnMore/LearnMore'
 
 export default function App() {
-  const routesWithNavbar = [
-    { path: "/", page: <Home /> },
-    { path: "/faq", page: <Faq /> }
-  ];
-
-  const routesWithoutNavbar = [
-    { path: "/learnMore", page: <LearnMore /> }
-  ];
-
-  const location = useLocation();
-  const showNavbar = routesWithNavbar.some(r => r.path === location.pathname);
-
-  const routeElements = [];
-
-  for (const r of routesWithNavbar) {
-    routeElements.push(<Route key={r.path} path={r.path} element={r.page} />);
-  }
-
-  for (const r of routesWithoutNavbar) {
-    routeElements.push(<Route key={r.path} path={r.path} element={r.page} />);
-  }
-
   return (
     <>
-      {showNavbar && <Navbar />}
-
+      <Navbar/>
       <main>
-        <Routes>{routeElements}</Routes>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/learnMore" element={<LearnMore/>}/>
+        </Routes>
       </main>
     </>
-  );
+  )
 }
