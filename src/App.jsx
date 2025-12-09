ae838216ee901d8181079862304c0aead53c0055
Victoria Iascevschi <victoria.iascevschi@student.nhlstenden.com>
2025-12-09 19:38:51 +0100

    Fixed App.jsx
    Modified App.jsx so that on all pages will be the same header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,21 @@
 import React from 'react'
-import { Routes, Route , useLocation} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
-import Faq from './pages/faq/Faq'
 import Navbar from './components/navbar/Navbar'
 import LearnMore from './pages/learnMore/LearnMore'
+import Studylandscape from './pages/studylandscape/Studylandscape'
 
 export default function App() {
-  const routesWithNavbar = [
-    { path: "/", page: <Home /> },
-    { path: "/faq", page: <Faq /> }
-  ];
-
-  const routesWithoutNavbar = [
-    { path: "/learnMore", page: <LearnMore /> }
-  ];
-
-  const location = useLocation();
-  const showNavbar = routesWithNavbar.some(r => r.path === location.pathname);
-
-  const routeElements = [];
-
-  for (const r of routesWithNavbar) {
-    routeElements.push(<Route key={r.path} path={r.path} element={r.page} />);
-  }
-
-  for (const r of routesWithoutNavbar) {
-    routeElements.push(<Route key={r.path} path={r.path} element={r.page} />);
-  }
-
   return (
     <>
-      {showNavbar && <Navbar />}
-
+      <Navbar/>
       <main>
-        <Routes>{routeElements}</Routes>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/learnMore" element={<LearnMore/>}/>
+          <Route path="/studylandscape" element={<Studylandscape/>}/>
+        </Routes>
       </main>
     </>
-  );
+  )
 }
---
+++ src/pages/studylandscape/Studylandscape.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './studylandscape.css';
+
+export default function Studylandscape() {
+    
+    return (
+        <section className="studylandscapePage">
+            <h1>Tech & Design</h1>
+        </section>
+    )
+}
