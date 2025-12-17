127b3ba0dd30f473734b64ef2367214eb9c1b19b
StefanBryda <stefan.bryda@student.nhlstenden.com>
2025-12-17 17:15:53 +0100

    Added student info path to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Navbar from './components/navbar/Navbar'
 import LearnMore from './pages/learnMore/LearnMore'
+import StudentInfo from './pages/studentInfo/studentinfo'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/learnMore" element={<LearnMore/>}/>
+          <Route path="/student-info" element={<StudentInfo/>}/>
         </Routes>
       </main>
     </>
