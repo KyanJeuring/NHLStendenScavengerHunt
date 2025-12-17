ddead951883c61ac2c876391343a9b29d2905613
StefanBryda <stefan.bryda@student.nhlstenden.com>
2025-12-17 17:12:12 +0100

    Added library path to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Navbar from './components/navbar/Navbar'
 import LearnMore from './pages/learnMore/LearnMore'
+import Library from './pages/library/Library'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/learnMore" element={<LearnMore/>}/>
+          <Route path="/library" element={<Library/>}/>
         </Routes>
       </main>
     </>
