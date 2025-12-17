7bdf37a799b54f932d3dacf4a76dc2e60706b449
Kyan Jeuring <kyanjeuring@gmail.com>
2025-12-17 11:53:28 +0100

    Added link to location card

--- src/components/locationcard/LocationCard.jsx
+++ src/components/locationcard/LocationCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './locationcard.css';
+import {useNavigate} from 'react-router-dom';
 
-export default function LocationCard({ image, title, description }) {
+export default function LocationCard({ image, title, description, link }) {
+    const navigate = useNavigate();
 
     return (
         <div className="location-card">
             <img src={image} alt="image" />
             <h2>{title}</h2>
             <p>{description}</p>
+            <p className="more-info" onClick={() => navigate(link)}>More information</p>
         </div>
     )
 }
