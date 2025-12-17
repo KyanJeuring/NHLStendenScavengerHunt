1aa7ab985ee688d3d4bbf2f7cb5dbf052e9734ac
Kyan Jeuring <kyanjeuring@gmail.com>
2025-12-17 12:02:54 +0100

    Added the url to the location card and made the whole card clickable

--- src/components/locationcard/LocationCard.jsx
+++ src/components/locationcard/LocationCard.jsx
@@ -6,11 +6,11 @@ export default function LocationCard({ image, title, description, link }) {
     const navigate = useNavigate();
 
     return (
-        <div className="location-card">
+        <div className="location-card" onClick={() => navigate(link)}>
             <img src={image} alt="image" />
             <h2>{title}</h2>
             <p>{description}</p>
-            <p className="more-info" onClick={() => navigate(link)}>More information</p>
+            <p className="more-info">More information</p>
         </div>
     )
 }
